741b05f61d74f51a2bafbdb7760b957ac4534147
Tran Dang Tin <thantienbut@gmail.com>
2022-06-16 21:18:35 +0700

    update src

--- src/tuan31/Bai1.java
+++ src/tuan31/Bai1.java
@@ -1,26 +1,6 @@
 package tuan31;
 
-import java.util.Scanner;
-
 public class Bai1 {
-    public static Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        System.out.print("Nhập số phần tử của mảng: ");
-        int n = scanner.nextInt();
-
-        int[] arr = new int[n];
-        System.out.print("Nhập các phần tử của mảng: \n");
-        for (int i = 0; i < n; i++) {
-            System.out.printf("a[%d] = ", i);
-            arr[i] = scanner.nextInt();
-        }
-        // sắp xếp dãy số theo thứ tự giảm dần
-        sortDESC(arr);
-        System.out.println("Dãy số được sắp xếp giảm dần: ");
-        show(arr);
-    }
-    
     public static String getKetQua(int[] arr) {
     	// sắp xếp dãy số theo thứ tự giảm dần
         sortDESC(arr);
--- src/tuan31/Bai5.java
+++ src/tuan31/Bai5.java
@@ -1,36 +1,6 @@
 package tuan31;
 
-import java.util.Scanner;
-
 public class Bai5 {
-    public static void main(String[] args) {
-        int n, i;
-        Scanner input = new Scanner(System.in);
-        System.out.printf("Nhap n= ");
-        n = input.nextInt();
-        int arr[] = new int[n];
-        System.out.println("Nhap vao cac phan tu cua mang:");
-        for (i = 0; i < n; i++)
-            arr[i] = input.nextInt();
-
-        int b = 0;
-        int c = 0;
-        for (i = 0; i < n; i++) {
-            if (check(arr[i])) {
-                b++;
-
-            } else {
-                if (arr[i] > 3) {
-                    c++;
-                }
-            }
-
-        }
-        System.out.println("Co " + b + " so nguyen to");
-        System.out.println("Co " + c + " hop so");
-
-    }
-    
     public static String getKetQua(int[] arr, int n) {
     	int i;
     	int b = 0;
